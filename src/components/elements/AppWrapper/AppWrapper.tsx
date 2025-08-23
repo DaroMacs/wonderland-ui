efa9230a90450ace7936ae16759ef4efaac0bdd7
DaroMacs <dariomacedog@gmail.com>
2025-08-23 14:05:02 -0600

    feat: add Globe component and integrate it into AppWrapper for the login page

--- src/components/elements/AppWrapper/AppWrapper.tsx
+++ src/components/elements/AppWrapper/AppWrapper.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { LOGIN } from "@/constants/routes";
 import ThemeRegistry from "@/lib/mui/themeRegistry";
 import { RainbowProvider } from "@/lib/walletConnection/providers";
 import Starfield from "@/styles/Starfield/Starfield";
 import { Box, Container } from "@mui/material";
+import { usePathname } from "next/navigation";
+import Globe from "../Globe";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const isLoginPage = pathname === LOGIN;
+
   return (
     <>
       <Starfield
@@ -31,6 +39,8 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
             >
               {children}
             </Container>
+
+            {isLoginPage && <Globe />}
           </Box>
         </ThemeRegistry>
       </RainbowProvider>
